8778051bbe496c1af2740f719d86e51fc9a5ef7e
Rohit Pokhrel <rohitpokhrel@ip-170-3.main.oberlin.edu>
2024-10-03 15:05:03 -0400

    Updated graph sketcher for special graph

--- graphSketcher.cpp
+++ graphSketcher.cpp
@@ -10,22 +10,30 @@
 
 int main() {
   typedef std::pair<int, int> Edge;
-  Edge k4_edges[] = {
+  Edge special_graph_edges[] = {
+    // k_4 edges
     Edge(0, 1),
     Edge(1, 2),
     Edge(2, 0),
     Edge(3, 1),
     Edge(3, 2),
     Edge(3, 0),
+    // k_3 edges
+    Edge(0, 4),
+    Edge(0, 5),
+    Edge(4, 5),
+    // k_2 edges
+    Edge(0, 6),
+    // special vertex 0: draw edge to every other vertex
   };
 
   using namespace boost;
   typedef adjacency_list<vecS, vecS, undirectedS, property<vertex_color_t, default_color_type>> Graph;
-  Graph k4(k4_edges, k4_edges + sizeof(k4_edges)/sizeof(Edge), 4);
+  Graph special_graph(special_graph_edges, special_graph_edges + sizeof(special_graph_edges)/sizeof(Edge), 7);
   typedef graph_traits<Graph>::vertex_descriptor Vertex;
   std::ofstream output_graph_stream;
-  output_graph_stream.open("k4_example.dot");
-  write_graphviz(output_graph_stream, k4);
+  output_graph_stream.open("special_example.dot");
+  write_graphviz(output_graph_stream, special_graph);
   output_graph_stream.close();
   return 0;
 }

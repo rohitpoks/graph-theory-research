b5517a52b40fe7d881257d3892030330fbeb434d
Rohit Pokhrel <rohitpokhrel@ip-169-225.main.oberlin.edu>
2024-11-21 15:04:32 -0500

    Added logic for original to coloring

--- driver.cpp
+++ driver.cpp
@@ -1,23 +1,11 @@
 #include "gtr.h"
 #include "graphSketcher.h"
 #include "graphProcessor.h"
+#include "originalToColoring.h"
 
 int main() {
-  Graph coloring_graph = create_graph();
-  print_graph_as_dot(coloring_graph, "coloring_graph.dot");
-  int special_vertex_int = 0;
-  Graph original_graph = find_original_graph(coloring_graph, 0);
-  print_graph_as_dot(original_graph, "original_graph.dot");
-  Vertex special_vertex = vertex(special_vertex_int, coloring_graph);
-  std::vector<std::vector<Vertex> > adjacentCompleteGraphs = find_adjacent_complete_graphs(special_vertex, coloring_graph);
-
-  for (auto& complete_graph: adjacentCompleteGraphs) {
-    std::cout << "new adjacent complete graph found with size " << complete_graph.size() << '\n';
-    for (auto& vertex: complete_graph) {
-      std::cout << vertex << " ";
-    }
-
-    std::cout << '\n';
-  }
+  Graph original_graph = create_graph();
+  Graph coloring_graph = coloringFromOriginal(original_graph, 8);
+  print_graph_as_dot(coloring_graph, "coloring_graph.dot"); 
   return 0;
 }
